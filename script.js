7312e4542d7b3d6211e9a2e525d4f39f305195b9
collijac000 <collijac000>
2020-02-20 10:29:53 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let question=document.getElementById("question");
+let o1=document.getElementById("btn0");
+let o2=document.getElementById("btn1");
+let o3=document.getElementById("btn2");
+let o4=document.getElementById("btn3");
+let progress=document.getElementById("progress");
+
+
+
+
+let doGame=function(){
+    let count=1;
+    
+
+
+
+
+    };
+
+
+
+let createQuestion=function(qt,c1,c2,c3,c4,cc){
+    question.innerText=qt;
+    o1.innerText=c1;
+    o2.innerText=c2;
+    o3.innerText=c3;
+    o4.innerText=c4;
+
+
+};
+
+//get the first set of questions
+//on click change questions
+//check if right answer
+    //if so add point
+    //else dont add point
+//
